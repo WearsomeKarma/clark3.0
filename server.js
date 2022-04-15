2be90c49f2b42ecd5ac0b07a9cd497ec28c3a535
WearsomeKarma <pahprios@gmail.com>
2022-04-15 15:20:28 -0400

    server.js 2

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-
+    res.sendFile("/index.html");
 });
 
 app.get('/user', function(req, res) {
-
+    
 });
 
 app.get('/user_edit', function(req, res) {
+    //is_auth
 
+    res.sendFile(__dirname + 'user_edit.html');
 });
+
+app.get('/user_edit.js', function(req, res) {
+    res.sendFile(__dirname + '/user_edit.js');
+})l
 
 app.get('/discussion', function(req, res) {
 
