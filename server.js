4e95238c612c18963c00aa8e8ab40363d4699ac3
WearsomeKarma <pahprios@gmail.com>
2022-04-15 15:16:23 -0400

    Initial server.js structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+
+});
+
+app.get('/user', function(req, res) {
+
+});
+
+app.get('/user_edit', function(req, res) {
+
+});
+
+app.get('/discussion', function(req, res) {
+
+});
+
+app.get('/get_discussions', function(req, res) {
+
+});
