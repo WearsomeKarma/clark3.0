d05567b553d9bb7ce3b4dccd4f25e0ed835d086b
WearsomeKarma <pahprios@gmail.com>
2022-04-26 17:10:49 -0400

    merge complete

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,13 +1,4 @@
 ///Show username if logged in
-$(document).ready(function(){
-    $.getJSON('/get_current_user').done(function (data){
-        if (data.message=== "success"){
-            //_if the user is logged in, remove the register and log in buttons
-            $('.login').remove();
-            $('#showname').text(data.user.username);
-        } else {
-            //_if the user is not logged in, then show the login and register buttons
-            $('.logout').remove()
-        }
-    })
-})
+
+apply_splash();
+apply_socials('#splash');
