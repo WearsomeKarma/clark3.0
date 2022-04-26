4b7321b7094f82057c2d4e45ce1cd3521821281b
Mahdi Boulila <mahdi8boulila@gmail.com>
2022-04-26 15:08:09 -0400

    Update index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,13 +3,11 @@ $(document).ready(function(){
     $.getJSON('/get_current_user').done(function (data){
         if (data.message=== "success"){
             //_if the user is logged in, remove the register and log in buttons
-            const user = data.user;
             $('.login').remove();
-            $('#showname').text(user.username);
+            $('#showname').text(data.user.username);
         } else {
             //_if the user is not logged in, then show the login and register buttons
             $('.logout').remove()
-            //something here
         }
     })
 })
