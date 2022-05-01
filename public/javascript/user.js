e453674f1c604d528a2c500dd47ee2e8f44720cf
Mahdi Boulila <mahdi8boulila@gmail.com>
2022-05-01 19:30:59 -0400

    commit user_settings

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -2,19 +2,42 @@
 const urlParams = new URLSearchParams(window.location.search);
 const user_id= urlParams.get("user_id")
 
-//Set user image and details
+//Set user image and details, authenticate the user for editing
 $('#user_img').attr('src', ("/img/default_user.png"));
 let current_user_id;
 $.getJSON("/get_current_user").done(function(data){
     current_user_id = data.user._id
-    console.log(current_user_id)
 })
+let user;
 $.getJSON("/get_user", {user_id: user_id}).done(function(data){
+    user = data.user;
     $("#username").text(data.user.username)
     $('#user_id').text(user_id)
     if (user_id !== current_user_id){
-        $("#settings").hide()
+        $("#settings").hide();
+        
     }
+})
+
+
+//hide and show showcases
+const post_list = $("#user_showcases");
+$("#showcase").on("click", function(){
+    if (post_list.hasClass("hidden")){
+        post_list.slideUp("1000");
+    } else {
+        post_list.slideDown("1000");
+    }
+    post_list.toggleClass("hidden");
+
+})
+
+//animate button group
+$(".button_user").on("click", function(){
+    $("#dashboard").children().removeClass("list-group-item-warning");
+    $("#dashboard").children().addClass("list-group-item-dark");
+    $(this).toggleClass("list-group-item-warning");
+    $(this).toggleClass("list-group-item-dark");
 })
 
 function make_post(post_dictionary){
@@ -37,6 +60,37 @@ function make_post(post_dictionary){
     `;
 }
 
+function make_settings_form(){
+return `
+    <li class="list-group-item bg-dark text-white border border-warning">
+        <form action="/user_edit" method="POST">
+            <div class="form-group">
+                <label for="email">New Email</label>
+                <input type="email" class="form-control" name="username" id="email" required>
+                <div> <p id="email_length_error" class="error_message py-0 my-0">hidden</p> </div>
+            </div>
+            <div class="form-group">
+                <label for="password">New Password</label>
+                <input type="password" class="form-control" name="password" id="password" required>
+                <div> <p id="password_length_error" class="error_message py-0 my-0">hidden</p> </div>
+            </div>
+            <div class="form-group">
+                <label for="confirm_password">Confirm Password</label>
+                <input type="password" class="form-control" name="confirm_password" id="confirm_password" required>
+                <div> <p id="password_length_error" class="error_message py-0 my-0">hidden</p> </div>
+            </div>
+            <div> <p id="error_message" class="error_message py-0 my-0"></p> </div>
+            <button type="submit" class="btn btn-dark mt-3">Submit Changes</button>
+        </form>     
+    </li>
+`;
+}
+
+function load_form(user){
+    $("#email").val(user.username);
+}
+
+//hardcoded templates, to be deleted
 const posts = [
     {
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -49,7 +103,19 @@ const posts = [
     
 ]
 
-const post_list = $("#user_showcases");
+//loading showcases
+post_list.hide();
 posts.forEach(function(post){
     post_list.append(make_post(post))
+})
+
+//loading settings
+$("#settings").on("click", function(){
+
+    //toggle change; if post_list visible make it hidden, and otherwise.
+    post_list.hide();
+    $("#user_settings").append(make_settings_form());
+    $("#user_settings").slideDown("500");
+
+    
 })
