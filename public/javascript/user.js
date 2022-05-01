fbf9ed5560a3dd15dcc9b40d4fbd9cc30701800c
Mahdi Boulila <mahdi8boulila@gmail.com>
2022-05-01 17:50:00 -0400

    push userpage

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -1,8 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const user_id= urlParams.get("user_id")
 
-function fill_user(user) {
-    $('#user_img').attr('src', (user?.img ?? "/img/default_user.png"));
-}
-
-fill_user($.getJSON("/get_user", {user_id: user_id}));
+$('#user_img').attr('src', ("/img/default_user.png"));
+$.getJSON("/get_current_user").done(function(data){
+    $("#username").text(data.user.username)
+    $('#user_id').text(user_id)
+})
